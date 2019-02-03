aa0750c79dc09f668d993193b6dbc3f1e3fbce9a
Inna Kuts <>
2019-02-03 11:57:33 +0200

    Initial commit

---
+++ AnimationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationTest
+//
+//  Created by Inna Kuts on 2/3/19.
+//  Copyright © 2019 Inna Kuts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
